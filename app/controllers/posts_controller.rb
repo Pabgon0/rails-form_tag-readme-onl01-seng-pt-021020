a039f11a94dd6e01646039dfd0bc48ce918dcec1
Pablo Gonzalez <pabgon0@gmail.com>
2020-06-17 20:21:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,11 @@ class PostsController < ApplicationController
   end
   
   def new
-<<<<<<< HEAD
 
   end
 
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
-=======
->>>>>>> bd46167b208c667061fb1673b16a64fed6bfa114
   end
 end
